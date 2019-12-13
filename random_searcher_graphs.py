49542dc687e3c842f8ce3a6454d7bf91890ade88
Parker J. Rule <parker.rule@tufts.edu>
2019-12-12 23:44:15 -0500

    Add finite-horizon search

--- random_searcher_graphs.py
+++ random_searcher_graphs.py
@@ -6,12 +6,13 @@ from person.random_walk_lost_person import RandomWalkLostPerson
 from person.shortest_path_lost_person import ShortestPathLostPerson
 from person.random_walk_searcher import RandomWalkSearcher
 import matplotlib.pyplot as plt
+from tqdm import trange
 
 
 def create_latency_vs_timestep(max_latency, number_samples, max_timestep):
     iterations = []
 
-    for i in range(max_latency):
+    for i in trange(max_latency):
         total = 0
         for j in range(number_samples):
 
@@ -33,12 +34,12 @@ def create_latency_vs_timestep(max_latency, number_samples, max_timestep):
         avg = total / number_samples
         iterations.append(avg)
 
-
-    plt.title("latency vs time step")
-    plt.plot(list(range(max_latency)),iterations)
-    plt.xlabel('latency')
-    plt.ylabel('time step')
-    plt.show()
+    plt.plot(iterations)
+    plt.xlabel('Latency')
+    plt.ylabel('Average number of search time steps')
+    plt.savefig('latency_vs_time_steps.pdf', bbox_inches='tight')
+    plt.close()
+    # plt.show()
 
 
 
@@ -46,7 +47,7 @@ def create_latency_vs_timestep(max_latency, number_samples, max_timestep):
 def create_latency_vs_percent_found(max_latency, number_samples, max_timestep):
     iterations = []
 
-    for i in range(max_latency):
+    for i in trange(max_latency):
         num_yes = 0
         for j in range(number_samples):
 
@@ -64,29 +65,28 @@ def create_latency_vs_percent_found(max_latency, number_samples, max_timestep):
 
             s = Scenario(m, [lp00], [searcher00], i)
             count = s.simulate(max_timestep)  # Simulate for N time steps
-            if count != max_timestep:
+            if s.num_rescued > 0:
                 num_yes += 1
         perc_found = num_yes / number_samples
         iterations.append(perc_found * 100)
 
-
-    plt.title("Latency vs Lost Percentage Found Rate")
-    plt.plot(list(range(max_latency)),iterations)
+    plt.plot(iterations)
     plt.xlabel('Latency')
-    plt.ylabel('Percent Found')
-    plt.show()
+    plt.ylabel('% lost persons found')
+    plt.savefig('latency_vs_percent_found.pdf', bbox_inches='tight')
+    plt.close()
+    # plt.show()
 
 
 
 def create_searchers_vs_time(max_searchers, latency, number_samples, num_time_steps):
     iterations = []
 
-    m = BasicMap(15, 15)
-    middle = (7,7)
-
-    for num_searchers in range(1, max_searchers):
+    for num_searchers in trange(1, max_searchers + 1):
         total = 0
         for i in range(number_samples):
+            m = BasicMap(15, 15)
+            middle = (7,7)
 
             # Add some lost persons to the map
             lp00 = RandomWalkLostPerson(m)
@@ -102,22 +102,21 @@ def create_searchers_vs_time(max_searchers, latency, number_samples, num_time_st
 
 
             s = Scenario(m, [lp00], searchers)
-            count = s.simulate(100)  # Simulate for N time steps
+            count = s.simulate(num_time_steps)  # Simulate for N time steps
             total += count
         avg = total / number_samples
 
         iterations.append(avg)
 
 
-    plt.title("searchers vs time step")
-    plt.plot(list(range(max_searchers - 1)),iterations)
-    plt.xlabel('searchers')
-    plt.ylabel('time step')
-    plt.show()
+    plt.plot(list(range(1, max_searchers + 1)), iterations)
+    plt.xlabel('Number of searchers')
+    plt.ylabel('Average number of search time steps')
+    plt.savefig('searchers_vs_time.pdf', bbox_inches='tight')
+    plt.close()
+    # plt.show()
 
 if __name__ == '__main__':
-    create_searchers_vs_time(10, 10, 10000, 100)
-    #create_latency_vs_percent_found(max_latency, number_samples, max_timestep):
-    #create_searchers_vs_time(10, 10, 100, 100)
-    #create_latency_vs_percent_found(100, 10000, 100)
-    create_searchers_vs_time(100, 60, 10000, 100)
+    create_latency_vs_timestep(100, 10000, 100)
+    create_latency_vs_percent_found(100, 10000, 100)
+    create_searchers_vs_time(20, 15, 10000, 100)
